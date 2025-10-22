affc3801f6b1bf1af0f1e8eaaec6f17bf968d01d
marcohoegen <93911665+marcohoegen@users.noreply.github.com>
2025-10-22 21:22:14 +0200

    Create basic endpoints for admin entity.

--- backend/src/admin/admin.controller.ts
+++ backend/src/admin/admin.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { AdminService } from './admin.service';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+
+  @Get()
+  findAll() {
+    return this.adminService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.adminService.findOne(+id);
+  }
+
+  @Post()
+  create(@Body() data: { name: string; email: string; password: string }) {
+    return this.adminService.create(data);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() data: { name?: string; email?: string; password?: string },
+  ) {
+    return this.adminService.update(+id, data);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.adminService.remove(+id);
+  }
+}
