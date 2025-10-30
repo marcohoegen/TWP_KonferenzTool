a7ed7caefa068b57d7d93d5317e5913088ab7607
Marco Högen <m.hoegen2001@gmail.com>
2025-10-30 09:50:36 +0100

    Changed Admin Entity (module, service and controller) and added Entity and Dto

--- backend/src/admin/admin.controller.ts
+++ backend/src/admin/admin.controller.ts
@@ -1,17 +1,16 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { CreateAdminDto } from './dto/create-admin.dto';
+import { UpdateAdminDto } from './dto/update-admin.dto';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
+
+  @Post()
+  create(@Body() createAdminDto: CreateAdminDto) {
+    return this.adminService.create(createAdminDto);
+  }
 
   @Get()
   findAll() {
@@ -23,17 +22,9 @@ export class AdminController {
     return this.adminService.findOne(+id);
   }
 
-  @Post()
-  create(@Body() data: { name: string; email: string; password: string }) {
-    return this.adminService.create(data);
-  }
-
   @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() data: { name?: string; email?: string; password?: string },
-  ) {
-    return this.adminService.update(+id, data);
+  update(@Param('id') id: string, @Body() updateAdminDto: UpdateAdminDto) {
+    return this.adminService.update(+id, updateAdminDto);
   }
 
   @Delete(':id')
--- backend/src/admin/admin.service.ts
+++ backend/src/admin/admin.service.ts
@@ -1,31 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import prisma from '../prisma/prisma';
+import { CreateAdminDto } from './dto/create-admin.dto';
+import { UpdateAdminDto } from './dto/update-admin.dto';
 
 @Injectable()
 export class AdminService {
+  create(createAdminDto: CreateAdminDto) {
+    return 'This action adds a new admin';
+  }
+
   findAll() {
-    return prisma.admin.findMany();
+    return `This action returns all admin`;
   }
 
   findOne(id: number) {
-    return prisma.admin.findUnique({ where: { id } });
+    return `This action returns a #${id} admin`;
   }
 
-  create(data: { name: string; email: string; password: string }) {
-    return prisma.admin.create({ data });
-  }
-
-  update(
-    id: number,
-    data: { name?: string; email?: string; password?: string },
-  ) {
-    return prisma.admin.update({
-      where: { id },
-      data,
-    });
+  update(id: number, updateAdminDto: UpdateAdminDto) {
+    return `This action updates a #${id} admin`;
   }
 
   remove(id: number) {
-    return prisma.admin.delete({ where: { id } });
+    return `This action removes a #${id} admin`;
   }
 }
