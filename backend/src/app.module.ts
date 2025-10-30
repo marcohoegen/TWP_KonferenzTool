aa896ef9efdd72bccd085a42db3721f74a653dc0
Marco Högen <m.hoegen2001@gmail.com>
2025-10-30 09:43:46 +0100

    Created rating and user structure (entity, Dto, module, service and controller) with generic responses.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
+import { UserModule } from './user/user.module';
+import { RatingModule } from './rating/rating.module';
 
 @Module({
-  imports: [AdminModule],
+  imports: [AdminModule, UserModule, RatingModule],
   controllers: [AppController],
   providers: [AppService],
 })
