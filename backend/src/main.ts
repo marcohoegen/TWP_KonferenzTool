75daf70d1cd5cebc195ae059e91b753e3685a5ae
Marco Högen <m.hoegen2001@gmail.com>
2025-10-30 10:59:44 +0100

    Added validation package.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,7 @@ async function bootstrap() {
     origin: 'http://localhost:5173',
     credentials: true,
   });
+  app.useGlobalPipes(new ValidationPipe({whitelist: true}))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
