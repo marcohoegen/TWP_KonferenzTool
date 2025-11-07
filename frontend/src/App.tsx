7e8d352e71cb8d54579fa586f8500e5f104288a9
Danielw1215 <danielwakker@gmail.com>
2025-11-07 16:23:58 +0100

    moved imported components to common

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import type { AdminData } from "./entities/adminEntity";
-import ButtonRoundedLgPrimaryBasic from "./components/ButtonRoundedLgPrimaryBasic";
+import ButtonRoundedLgPrimaryBasic from "./common/ButtonRoundedLgPrimaryBasic";
 import AdminSeite from "./pages/AdminLogin";
 import CustomerSeite from "./pages/CustomerSeite";
 import confeedlogo from "./assets/confeedlogo.svg";
