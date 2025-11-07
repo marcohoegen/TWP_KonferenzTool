b42b9a9892d0247be592ca8791663761d5225d63
Danielw1215 <danielwakker@gmail.com>
2025-11-07 19:08:30 +0100

    added Admin Login input mask and removed AI artifacts in App.tsx

--- frontend/src/pages/AdminLogin.tsx
+++ frontend/src/pages/AdminLogin.tsx
@@ -1,8 +1,18 @@
+import ButtonRoundedFullBasePrimaryBasic from "../common/ButtonRoundedFullBasePrimaryBasic";
+import FormElementsInputPlainBaseTrailingIcon from "../common/FormElementsInputPlainBaseTrailingIcon";
+import FormElementsInputRoundedBaseBasic from "../common/FormElementsInputRoundedBaseBasic";
+import confeedlogo from "../assets/confeedlogo.svg";
+
 const AdminLogin = () => {
   return (
     <div>
+      <img src={confeedlogo} alt="Confeed Logo" />
       <h1>Admin-login</h1>
-      <p>Hier kommt das Admin Login hin.</p>
+      <FormElementsInputRoundedBaseBasic id="email" label="Email" />
+      <FormElementsInputPlainBaseTrailingIcon />
+      <ButtonRoundedFullBasePrimaryBasic>
+        Einloggen
+      </ButtonRoundedFullBasePrimaryBasic>
     </div>
   );
 };
