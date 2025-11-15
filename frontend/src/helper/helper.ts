0287dd0e6485c3f04d765a1a3238e7eea8c86684
marcohoegen <93911665+marcohoegen@users.noreply.github.com>
2025-11-15 20:43:55 +0100

    Added cropEmail helper function.

--- frontend/src/helper/helper.ts
+++ frontend/src/helper/helper.ts
@@ -8,3 +8,12 @@ export function formatDate(dateStr: string): string {
     year: "numeric",
   });
 }
+
+export function cropEmail(emailStr: string, chars: number): string {
+  if (emailStr.length > chars) {
+    emailStr = emailStr.substring(0, chars - 3);
+    emailStr = emailStr.concat("...");
+  }
+
+  return emailStr;
+}
