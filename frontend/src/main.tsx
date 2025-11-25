5934a5c8e4cb680e9747d20ab5cbc104fbba20f5
Danielw1215 <danielwakker@gmail.com>
2025-11-26 00:00:37 +0100

    remove RatingProvider from main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,14 +4,11 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./api/queryClient.ts";
-import { RatingProvider } from "./context/RatingContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RatingProvider>
-        <App />
-      </RatingProvider>
+      <App />
     </QueryClientProvider>
   </StrictMode>
 );
